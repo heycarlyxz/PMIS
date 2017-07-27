5c7bac55282f1daf1f1846630097c34a7f8bb913
Rannel Bravante <lennarbravante@gmail.com>
2017-07-27 14:00:27 +0800

    Created DB

---
+++ PMIS/PMIS.DAL/DB.cs
@@ -0,0 +1,19 @@
+﻿using PMIS.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMIS.DAL
+{
+    public class DB : DbContext
+
+    {
+        public DB() : base("DefaultConnection") { }
+
+        public DbSet<Users> Users { get; set; }
+        
+    }
+}
---
+++ PMIS/PMIS.Entities/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PMIS.Entities
+{
+    public class Users
+    {
+        [Key]
+        public int Id { get; set; }
+        public Guid UniqueId { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmployeeId { get; set; }
+    }
+}
